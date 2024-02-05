acc29bc104e3cd68fa62867c63bcdbf68b9cf57a
rival <rivaljyoung@gmail.com>
2024-02-04 22:15:38 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int intOperandA = 12;
+        int intOperandB = 31;
+        int intSum = 0;
+        int intProduct = 0;
+        int intQuotient = 0;
+        int intDifference = 0;
+        int intModulo = 0; // remainder, uses the %
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("the sum of " + intOperandA + " and " + intOperandB + " = " + intSum);
+
+
+
+    }
+}
